21866a23da6e39abeba136971e8f55322c214302
gethub <panga@LAPTOP-51O8584Q>
2026-09-22 21:56:59 +0800

    ROOM MAINTENANCE (UI)

---
+++ UI/RoomMaintenanceForm.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace UI
+{
+    partial class RoomMaintenanceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblDashboard = new Label();
+            btnRoomTypesandRates = new Button();
+            btnRoomMaintenance = new Button();
+            btnBullingPayment = new Button();
+            btnServicesAddons = new Button();
+            btnGuests = new Button();
+            btnRooms = new Button();
+            btnCheckinCheckOut = new Button();
+            btnLogout = new Button();
+            btnReports = new Button();
+            btnUserRoles = new Button();
+            btnSettings = new Button();
+            btnReaervation = new Button();
+            lblRoomsMaintenance = new Label();
+            listRoomon = new ListView();
+            listView2 = new ListView();
+            listView3 = new ListView();
+            listView4 = new ListView();
+            listView5 = new ListView();
+            btnAddNewRooms = new Button();
+            listView6 = new ListView();
+            lblRoomon = new Label();
+            lblRoomtype = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // lblDashboard
+            // 
+            lblDashboard.AutoSize = true;
+            lblDashboard.Location = new Point(12, 21);
+            lblDashboard.Name = "lblDashboard";
+            lblDashboard.Size = new Size(100, 25);
+            lblDashboard.TabIndex = 0;
+            lblDashboard.Text = "Dashboard";
+            // 
+            // btnRoomTypesandRates
+            // 
+            btnRoomTypesandRates.Location = new Point(47, 134);
+            btnRoomTypesandRates.Name = "btnRoomTypesandRates";
+            btnRoomTypesandRates.Size = new Size(215, 34);
+            btnRoomTypesandRates.TabIndex = 2;
+            btnRoomTypesandRates.Text = "Room Types & Rates";
+            btnRoomTypesandRates.UseVisualStyleBackColor = true;
+            // 
+            // btnRoomMaintenance
+            // 
+            btnRoomMaintenance.Location = new Point(47, 184);
+            btnRoomMaintenance.Name = "btnRoomMaintenance";
+            btnRoomMaintenance.Size = new Size(215, 34);
+            btnRoomMaintenance.TabIndex = 3;
+            btnRoomMaintenance.Text = "Room Maintenance";
+            btnRoomMaintenance.UseVisualStyleBackColor = true;
+            // 
+            // btnBullingPayment
+            // 
+            btnBullingPayment.Location = new Point(47, 340);
+            btnBullingPayment.Name = "btnBullingPayment";
+            btnBullingPayment.Size = new Size(183, 34);
+            btnBullingPayment.TabIndex = 6;
+            btnBullingPayment.Text = "Bulling /Payment";
+            btnBullingPayment.UseVisualStyleBackColor = true;
+            // 
+            // btnServicesAddons
+            // 
+            btnServicesAddons.Location = new Point(47, 448);
+            btnServicesAddons.Name = "btnServicesAddons";
+            btnServicesAddons.Size = new Size(196, 34);
+            btnServicesAddons.TabIndex = 10;
+            btnServicesAddons.Text = "Services (Add-ons )";
+            btnServicesAddons.UseVisualStyleBackColor = true;
+            // 
+            // btnGuests
+            // 
+            btnGuests.Location = new Point(47, 287);
+            btnGuests.Name = "btnGuests";
+            btnGuests.Size = new Size(112, 34);
+            btnGuests.TabIndex = 12;
+            btnGuests.Text = "Guests";
+            btnGuests.UseVisualStyleBackColor = true;
+            btnGuests.Click += button12_Click;
+            // 
+            // btnRooms
+            // 
+            btnRooms.Location = new Point(47, 94);
+            btnRooms.Name = "btnRooms";
+            btnRooms.Size = new Size(112, 34);
+            btnRooms.TabIndex = 14;
+            btnRooms.Text = "Rooms";
+            btnRooms.UseVisualStyleBackColor = true;
+            // 
+            // btnCheckinCheckOut
+            // 
+            btnCheckinCheckOut.Location = new Point(47, 394);
+            btnCheckinCheckOut.Name = "btnCheckinCheckOut";
+            btnCheckinCheckOut.Size = new Size(196, 34);
+            btnCheckinCheckOut.TabIndex = 15;
+            btnCheckinCheckOut.Text = "Check-in /Check-Out";
+            btnCheckinCheckOut.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            btnLogout.Location = new Point(47, 665);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(112, 34);
+            btnLogout.TabIndex = 16;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // btnReports
+            // 
+            btnReports.Location = new Point(47, 555);
+            btnReports.Name = "btnReports";
+            btnReports.Size = new Size(112, 34);
+            btnReports.TabIndex = 17;
+            btnReports.Text = "Reports";
+            btnReports.UseVisualStyleBackColor = true;
+            // 
+            // btnUserRoles
+            // 
+            btnUserRoles.Location = new Point(47, 498);
+            btnUserRoles.Name = "btnUserRoles";
+            btnUserRoles.Size = new Size(112, 34);
+            btnUserRoles.TabIndex = 18;
+            btnUserRoles.Text = "User & Roles";
+            btnUserRoles.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            btnSettings.Location = new Point(47, 609);
+            btnSettings.Name = "btnSettings";
+            btnSettings.Size = new Size(112, 34);
+            btnSettings.TabIndex = 19;
+            btnSettings.Text = "Settings";
+            btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // btnReaervation
+            // 
+            btnReaervation.Location = new Point(47, 236);
+            btnReaervation.Name = "btnReaervation";
+            btnReaervation.Size = new Size(112, 34);
+            btnReaervation.TabIndex = 20;
+            btnReaervation.Text = "Reaervation";
+            btnReaervation.UseVisualStyleBackColor = true;
+            // 
+            // lblRoomsMaintenance
+            // 
+            lblRoomsMaintenance.AutoSize = true;
+            lblRoomsMaintenance.Location = new Point(277, 40);
+            lblRoomsMaintenance.Name = "lblRoomsMaintenance";
+            lblRoomsMaintenance.Size = new Size(173, 25);
+            lblRoomsMaintenance.TabIndex = 21;
+            lblRoomsMaintenance.Text = "Rooms Maintenance";
+            // 
+            // listRoomon
+            // 
+            listRoomon.Location = new Point(354, 94);
+            listRoomon.Name = "listRoomon";
+            listRoomon.Size = new Size(182, 470);
+            listRoomon.TabIndex = 22;
+            listRoomon.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            listView2.Location = new Point(1283, 94);
+            listView2.Name = "listView2";
+            listView2.Size = new Size(182, 470);
+            listView2.TabIndex = 23;
+            listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView3
+            // 
+            listView3.Location = new Point(1049, 94);
+            listView3.Name = "listView3";
+            listView3.Size = new Size(182, 470);
+            listView3.TabIndex = 24;
+            listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView4
+            // 
+            listView4.Location = new Point(816, 94);
+            listView4.Name = "listView4";
+            listView4.Size = new Size(182, 470);
+            listView4.TabIndex = 25;
+            listView4.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView5
+            // 
+            listView5.Location = new Point(586, 94);
+            listView5.Name = "listView5";
+            listView5.Size = new Size(182, 470);
+            listView5.TabIndex = 26;
+            listView5.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnAddNewRooms
+            // 
+            btnAddNewRooms.Location = new Point(1565, 31);
+            btnAddNewRooms.Name = "btnAddNewRooms";
+            btnAddNewRooms.Size = new Size(182, 34);
+            btnAddNewRooms.TabIndex = 27;
+            btnAddNewRooms.Text = "Add New Rooms";
+            btnAddNewRooms.UseVisualStyleBackColor = true;
+            // 
+            // listView6
+            // 
+            listView6.Location = new Point(1522, 94);
+            listView6.Name = "listView6";
+            listView6.Size = new Size(182, 470);
+            listView6.TabIndex = 28;
+            listView6.UseCompatibleStateImageBehavior = false;
+            // 
+            // lblRoomon
+            // 
+            lblRoomon.AutoSize = true;
+            lblRoomon.Location = new Point(354, 83);
+            lblRoomon.Name = "lblRoomon";
+            lblRoomon.Size = new Size(86, 25);
+            lblRoomon.TabIndex = 29;
+            lblRoomon.Text = "Room on";
+            // 
+            // lblRoomtype
+            // 
+            lblRoomtype.AutoSize = true;
+            lblRoomtype.Cursor = Cursors.SizeNESW;
+            lblRoomtype.Location = new Point(586, 83);
+            lblRoomtype.Name = "lblRoomtype";
+            lblRoomtype.Size = new Size(102, 25);
+            lblRoomtype.TabIndex = 30;
+            lblRoomtype.Text = "Room Type";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(816, 83);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 25);
+            label3.TabIndex = 31;
+            label3.Text = "Rate";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(1049, 83);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 25);
+            label4.TabIndex = 32;
+            label4.Text = "Capacity";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(1283, 83);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 25);
+            label5.TabIndex = 33;
+            label5.Text = "Satus";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(1522, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 25);
+            label1.TabIndex = 34;
+            label1.Text = "Actions";
+            // 
+            // RoomMaintenanceForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1776, 798);
+            Controls.Add(label1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(lblRoomtype);
+            Controls.Add(lblRoomon);
+            Controls.Add(listView6);
+            Controls.Add(btnAddNewRooms);
+            Controls.Add(listView5);
+            Controls.Add(listView4);
+            Controls.Add(listView3);
+            Controls.Add(listView2);
+            Controls.Add(listRoomon);
+            Controls.Add(lblRoomsMaintenance);
+            Controls.Add(btnReaervation);
+            Controls.Add(btnSettings);
+            Controls.Add(btnUserRoles);
+            Controls.Add(btnReports);
+            Controls.Add(btnLogout);
+            Controls.Add(btnCheckinCheckOut);
+            Controls.Add(btnRooms);
+            Controls.Add(btnGuests);
+            Controls.Add(btnServicesAddons);
+            Controls.Add(btnBullingPayment);
+            Controls.Add(btnRoomMaintenance);
+            Controls.Add(btnRoomTypesandRates);
+            Controls.Add(lblDashboard);
+            Name = "RoomMaintenanceForm";
+            Text = "RoomMaintenanceForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblDashboard;
+        private Button btnCheckinCheckOut;
+        private Button btnRoomTypesandRates;
+        private Button btnRoomMaintenance;
+        private Button btnUserRoles;
+        private Button btnSettings;
+        private Button btnBullingPayment;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button btnServicesAddons;
+        private Button button11;
+        private Button btnGuests;
+        private Button btnRooms;
+        private Button btnLogout;
+        private Button btnReports;
+        private Button btnReaervation;
+        private Label lblRoomsMaintenance;
+        private ListView listRoomon;
+        private ListView listView2;
+        private ListView listView3;
+        private ListView listView4;
+        private ListView listView5;
+        private Button btnAddNewRooms;
+        private ListView listView6;
+        private Label lblRoomon;
+        private Label lblRoomtype;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label1;
+    }
+}
