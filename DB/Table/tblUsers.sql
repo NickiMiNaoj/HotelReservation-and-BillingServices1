7687b70a6fb1e94ae4da7a1d7ee4fb23ad2ecd97
Username <User@qweqwe>
2026-09-27 03:49:18 +0800

    Login Database

---
+++ DB/Table/tblUsers.sql
@@ -0,0 +1,8 @@
+﻿-- Create tblUsers Table
+CREATE TABLE tblUsers (
+    UserID INT IDENTITY(1,1) PRIMARY KEY,
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    Password NVARCHAR(250) NOT NULL, -- Stored as text/hash
+    FullName NVARCHAR(100) NOT NULL,
+    Role NVARCHAR(20) NOT NULL -- e.g., 'Admin', 'Front Desk'
+);
